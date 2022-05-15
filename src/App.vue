7a94a9b6923ac81d5e5e1cbc3bf469e07e013e58
千迦 <qianjia.work@gmail.com>
2022-05-15 20:32:12 +0800

    引入axios 登入頁面貼模板
    npm vue axios
    把main.js裡面格式調整成他要的方式
    貼上登入的模板

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,6 @@
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
-  <button type="button" class="btn btn-primary">Primary</button>
-  <button type="button" class="btn btn-secondary">Secondary</button>
-  <button type="button" class="btn btn-success">Success</button>
-  <button type="button" class="btn btn-danger">Danger</button>
-  <button type="button" class="btn btn-warning">Warning</button>
-  <button type="button" class="btn btn-info">Info</button>
-  <button type="button" class="btn btn-light">Light</button>
-  <button type="button" class="btn btn-dark">Dark</button>
-  <button type="button" class="btn btn-link">Link</button>
 </template>
 
 <style lang="scss">
---
+++ src/views/LoginView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container mt-5">
+        <form class="row justify-content-center">
+        <div class="col-md-6">
+            <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
+            <div class="mb-2">
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input
+                type="email"
+                id="inputEmail"
+                class="form-control"
+                placeholder="Email address"
+                required
+                autofocus
+            />
+            </div>
+            <div class="mb-2">
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input
+                type="password"
+                id="inputPassword"
+                class="form-control"
+                placeholder="Password"
+                required
+            />
+            </div>
+            <div class="text-end mt-4">
+            <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+            </div>
+        </div>
+        </form>
+    </div>
+</template>
